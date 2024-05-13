40aad3b6892b7c4ef7bf76a54b4b7c437d8067fb
SilviaCue <152323429+SilviaCue@users.noreply.github.com>
2024-05-13 13:25:26 +0200

    pendiente revisar css

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
-from PIL import Image
+import io
+import os
+import torch
 from diffusers import DiffusionPipeline
+from PIL import Image
+from flask import Flask, render_template, request
 import base64
 
 app = Flask(__name__)
@@ -9,34 +12,37 @@ app = Flask(__name__)
 arte = DiffusionPipeline.from_pretrained(
     "lambdalabs/sd-image-variations-diffusers")
 
-# Ruta inicial
+# Definir la ruta para la página principal
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Ruta para procesar las imágenes seleccionadas
+# Definir la ruta para procesar las imágenes
 
 
 @app.route('/procesar', methods=['POST'])
 def procesar_imagenes():
-    if 'file' not in request.files:
-        return 'No se seleccionó ninguna imagen.'
+    # Obtener las imágenes del formulario
+    archivos = request.files.getlist('file')
 
-    # Obtener la imagen del formulario
-    archivo = request.files['file']
-    imagen = Image.open(archivo)
+    # Procesar cada imagen individualmente
+    imagenes_procesadas = []
+    for archivo in archivos:
+        imagen = Image.open(archivo)
 
-    # Procesar la imagen utilizando el modelo de difusión
-    imagen_procesada = arte(imagen)
+        # Procesar la imagen utilizando el modelo de difusión
+        imagen_procesada = arte(imagen)
 
-    # Convertir la imagen procesada a base64 para mostrarla en el navegador
-    buffered = io.BytesIO()
-    imagen_procesada.save(buffered, format="JPEG")
-    imagen_codificada = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        # Convertir la imagen procesada a base64
+        buffered = io.BytesIO()
+        imagen_procesada.save(buffered, format="JPEG")
+        imagen_codificada = base64.b64encode(
+            buffered.getvalue()).decode("utf-8")
+        imagenes_procesadas.append(imagen_codificada)
 
-    return render_template('resultado.html', imagen=imagen_codificada)
+    return render_template('index.html', imagenes=imagenes_procesadas)
 
 
 if __name__ == '__main__':
